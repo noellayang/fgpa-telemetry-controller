bb3d95eb4e3267d3506f8162ce4042116b01fa85
noellayang <noellay2007@gmail.com>
2026-08-30 18:40:25 -0400

    Create sample timer

--- rtl/sample_timer.v
+++ rtl/sample_timer.v
@@ -1,1 +1,29 @@
+module timer(
+    input clk,
+    input reset,
+    output reg sample_tick
+);
 
+reg [2:0] counter;
+    
+always@(posedge clk) begin
+    
+    if (reset) begin
+        sample_tick <= 0;
+        counter <= 0;
+    end
+
+    else if (counter == 4) begin
+        sample_tick <= 1;
+        counter <= 0;
+    end
+    
+    else begin
+        sample_tick <= 0;
+        counter <= counter + 1;
+    end 
+
+
+end
+ 
+endmodule
